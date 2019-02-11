1a8e787cc1172000f4c97049e1aa385a6135f9d2
ArpMichaelB <40207147+ArpMichaelB@users.noreply.github.com>
2019-02-11 10:39:03 -0600

    General Form Structure
    Just the general structure of the contact form. It'll get more nuanced as time goes by.

--- project-fds/src/pages/Contact/Contact.js
+++ project-fds/src/pages/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Form, Input, Button} from "antd";
 
 import "./style.css";
 
@@ -12,7 +13,32 @@ const Contact = () => {
 			
 			</div>
 			<div className="contact_info_form_wrapper">
-			
+				<Form>
+					<Form.Item layout="vertical">
+						<Input className="contact_info_form_input" placeholder="First Name"/>
+					</Form.Item>
+					<Form.Item layout="vertical">
+						<Input className="contact_info_form_input" placeholder="Last Name"/>
+					</Form.Item>
+					<Form.Item layout="vertical">
+						<Input className="contact_info_form_input" placeholder="E-Mail"/>
+					</Form.Item>
+					<Form.Item layout="vertical">
+						<Input className="contact_info_form_input" placeholder="Phone Number"/>
+					</Form.Item>
+					<Form.Item layout="vertical">
+						<Input className="contact_info_form_input" placeholder="Your Message"/>
+					</Form.Item>
+					<Form.Item>
+						<Button
+							type="primary"
+							htmlType="submit"
+							className="contact_info_form_button"
+						>
+							Send Message
+						</Button>
+					</Form.Item>
+				</Form>
 			</div>
 		</main>
 	);
