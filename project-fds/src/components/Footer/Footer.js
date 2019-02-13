d6f6442710d0764ce666e140ad84a9ab1c630e2d
Zachary Karamanlis <zachary.karamanlis@gmail.com>
2019-02-13 14:33:22 -0600

    fixed copyright

--- project-fds/src/components/Footer/Footer.js
+++ project-fds/src/components/Footer/Footer.js
@@ -15,7 +15,7 @@ class Footer extends Component {
 
 			<div id="footer">
 				<p>Email: joeerenberger@gmail.com | Phone: (309)-292-2777 | Address: 230 West 3rd St., Suite 216 Davenport, IA, 52801</p>
-				<span>Copyright <Icon type="copyright" /> Yash Technologies 2019-{(new Date().getFullYear())}</span>
+				<span>Copyright <Icon type="copyright" /> Insight Manegment 2019-{(new Date().getFullYear())}</span>
 			</div>
 
 		);
