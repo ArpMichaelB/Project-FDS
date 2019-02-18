5410458a53e6274bedd6a7df552785676441bce1
Zachary Karamanlis <zachary.karamanlis@gmail.com>
2019-02-18 11:41:49 -0600

    fixed error with default routing

--- project-fds/src/components/NavBar/NavBar.js
+++ project-fds/src/components/NavBar/NavBar.js
@@ -1,26 +1,31 @@
 import React, { Component } from "react";
 import { Menu } from "antd";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import Sec from "../../hoc/sec/Sec";
 import logo from "../../assets/images/logo.jpg";
+import PropTypes from "prop-types";
 
 import "./style.css";
 
 class NavBar extends Component {
+
+	static propTypes = {
+		history:PropTypes.object
+	}
 
 	state = {
 		current: "home",
 	}
 	
 	handleClick = (e) => {
-		//console.log("click ", e);
 		this.setState({
 			current: e.key,
 		});
 	}
 
-	goToLink = (link) =>{
-		document.getElementById(link).click();
+	goToLink = (link, key) =>{
+		this.props.history.push(link);
+		this.setState({current:key});
 	}
 
 	render() {
@@ -45,15 +50,15 @@ class NavBar extends Component {
 							<Menu.Item key="Gallery">
 								<Link to="/gallery">Gallery</Link>
 							</Menu.Item>
-							<SubMenu onTitleClick={() => this.goToLink("available-properties")} title={<span className="submenu-title-wrapper">Properties</span>}>
+							<SubMenu onTitleClick={() => this.goToLink("/available-properties", "setting:1")} title={<span className="submenu-title-wrapper">Properties</span>}>
 								<MenuItemGroup>
-									<Menu.Item key="setting:1"><Link id="available-properties" to="/available-properties">Available Properties</Link></Menu.Item>
+									<Menu.Item key="setting:1"><Link to="/available-properties">Available Properties</Link></Menu.Item>
 									<Menu.Item key="setting:2"><Link to="/properties">All Properties</Link></Menu.Item>
 								</MenuItemGroup>
 							</SubMenu>
-							<SubMenu onTitleClick={() => this.goToLink("tenant-maintenance")} title={<span className="submenu-title-wrapper">Maintenance</span>}>
+							<SubMenu onTitleClick={() => this.goToLink("/tenant-maintenance", "setting:3")} title={<span className="submenu-title-wrapper">Maintenance</span>}>
 								<MenuItemGroup>
-									<Menu.Item key="setting:3"><Link id="tenant-maintenance" to="/tenant-maintenance">Tenant</Link></Menu.Item>
+									<Menu.Item key="setting:3"><Link to="/tenant-maintenance">Tenant</Link></Menu.Item>
 									<Menu.Item key="setting:4"><Link to="/employee-maintenance">Employee</Link></Menu.Item>
 								</MenuItemGroup>
 							</SubMenu>
@@ -72,4 +77,4 @@ class NavBar extends Component {
 }
 
 
-export default NavBar;
+export default withRouter(NavBar);
