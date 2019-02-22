1db52bcdc7e7507054fc88c3b186d9908fca4a43
Zachary Karamanlis <zachary.karamanlis@gmail.com>
2019-02-22 10:55:07 -0600

    resets scroll bar when switching pages

--- project-fds/src/index.js
+++ project-fds/src/index.js
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import ScrollFix from "./components/ScrollFix/ScrollFix";
 
 
 const app = (
 	<BrowserRouter>
-		<App />
+		<ScrollFix>
+			<App />
+		</ScrollFix>
 	</BrowserRouter>
 );
 
