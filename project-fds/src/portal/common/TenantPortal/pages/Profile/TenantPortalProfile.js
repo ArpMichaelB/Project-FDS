2a19054606fc0a0d31526b2ad84ba4b69a4a9925
Zachary Karamanlis <zachary.karamanlis@gmail.com>
2019-03-07 13:13:33 -0600

    added starter cards to tenant pages

--- project-fds/src/portal/common/TenantPortal/pages/Profile/TenantPortalProfile.js
+++ project-fds/src/portal/common/TenantPortal/pages/Profile/TenantPortalProfile.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
+import { Card } from "antd";
+
+import "./style.css";
 
 class TenantPortalProfile extends Component {
 
 	render(){
 		return(
-			<div>
-				Profile
-			</div>
+			<Card title="Profile" bordered={false} className="profile_card">
+				<h2>Payment Form</h2>
+			</Card>
 		);
 	}
 }
