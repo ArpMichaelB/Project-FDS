8fcb914413d4ff3265bc47bf83dc7fbfcfbcd0e4
Lhito Camson <lhito.camson@drake.edu>
2019-03-20 10:27:56 -0500

    added employee maintenance form, but need to finish displaying and passing data from the employee maintenance portal before i can finish this branch

--- project-fds/src/portal/common/MaintenancePortal/pages/Detail/MaintenancePortalDetail.js
+++ project-fds/src/portal/common/MaintenancePortal/pages/Detail/MaintenancePortalDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import EmployeeMaintenance from "../../../../../pages/EmployeeMaintenance/EmployeeMaintenance";
 import { Card, Button } from "antd";
 import { withRouter } from "react-router-dom";
 
@@ -14,12 +15,15 @@ class MaintenancePortalDetail extends Component {
 		};
 	}
 
-	render(){
-		return(
-			<Card title="Maintenance Request Details" bordered={false} id="maintenance_data">
-				<h3>Details for {this.props.match.params.tenantName}</h3>
-				<Button onClick={() => this.props.history.push("/portal/maintenance/home")} className="basic_button">Back</Button>
-			</Card>
+	render() {
+		return (
+			<React.Fragment>
+				<Card title="Maintenance Request Details" bordered={false} id="maintenance_data">
+					<h3>Details for {this.props.match.params.tenantName}</h3>
+					<Button onClick={() => this.props.history.push("/portal/maintenance/home")} className="basic_button">Back</Button>
+				</Card>
+				<EmployeeMaintenance />
+			</React.Fragment>
 		);
 	}
 }
